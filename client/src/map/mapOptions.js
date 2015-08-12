49231c1a12f2ea362b55c876bd158fe37cd8bf04
Rodolfo Yabut <rodolfoyabut@gmail.com>
2015-08-11 23:41:28 -0700

    [Fix] Remove blue tint from map

--- client/src/map/mapOptions.js
+++ client/src/map/mapOptions.js
@@ -1,158 +1,85 @@
 var map = angular.module('parkAssist.map');
 
-map.factory('MapOptions', [function() {
-  return {
-    zoom: 17,
-    minZoom: 3,
-    maxZoom: 25,
-    center: {
-      lat: 34.0219,
-      lng: -118.4814
-    },
-    styles: [{
-      "featureType": "all",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#d6d9dd"
-      }]
-    }, {
-      "featureType": "all",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "gamma": 0.01
-      }, {
-        "lightness": 20
-      }]
-    }, {
-      "featureType": "all",
-      "elementType": "labels.text.stroke",
-      "stylers": [{
-        "saturation": -31
-      }, {
-        "lightness": -33
-      }, {
-        "weight": 2
-      }, {
-        "gamma": 0.8
-      }]
-    }, {
-      "featureType": "all",
-      "elementType": "labels.icon",
-      "stylers": [{
-        "visibility": "off"
-      }]
-    }, {
-      "featureType": "administrative.land_parcel",
-      "elementType": "geometry",
-      "stylers": [{
-        "saturation": "8"
-      }]
-    }, {
-      "featureType": "administrative.land_parcel",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "saturation": "-4"
-      }]
-    }, {
-      "featureType": "landscape",
-      "elementType": "geometry",
-      "stylers": [{
-        "lightness": 30
-      }, {
-        "saturation": 30
-      }]
-    }, {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [{
-        "saturation": 20
-      }]
-    }, {
-      "featureType": "poi.park",
-      "elementType": "geometry",
-      "stylers": [{
-        "lightness": 20
-      }, {
-        "saturation": -20
-      }]
-    }, {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [{
-        "lightness": 10
-      }, {
-        "saturation": -30
-      }]
-    }, {
-      "featureType": "road",
-      "elementType": "geometry.fill",
-      "stylers": [{
-        "lightness": "-8"
-      }]
-    }, {
-      "featureType": "road",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "saturation": 25
-      }, {
-        "lightness": 25
-      }]
-    }, {
-      "featureType": "road.highway",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "lightness": "0"
-      }]
-    }, {
-      "featureType": "road.local",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "weight": "0.01"
-      }, {
-        "saturation": "5"
-      }, {
-        "lightness": "20"
-      }]
-    }, {
-      "featureType": "road.local",
-      "elementType": "labels.text.stroke",
-      "stylers": [{
-        "visibility": "on"
-      }, {
-        "weight": "0.57"
-      }, {
-        "saturation": "-13"
-      }]
-    }, {
-      "featureType": "road.local",
-      "elementType": "labels.icon",
-      "stylers": [{
-        "visibility": "simplified"
-      }, {
-        "weight": "0.52"
-      }, {
-        "saturation": "-46"
-      }, {
-        "lightness": "-6"
-      }, {
-        "gamma": "0.47"
-      }, {
-        "hue": "#ff0000"
-      }]
-    }, {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [{
-        "lightness": -20
-      }]
-    }, {
-      "featureType": "water",
-      "elementType": "geometry.fill",
-      "stylers": [{
-        "hue": "#008bff"
-      }, {
-        "saturation": "23"
-      }]
-    }]
-  };
-}]);
+map.factory('MapOptions', [
+
+    function() {
+        return {
+            zoom: 17,
+            minZoom: 3,
+            maxZoom: 25,
+            center: {
+                lat: 34.0219,
+                lng: -118.4814
+            },
+            styles: [{
+                "stylers": [{
+                    "visibility": "on"
+                }, {
+                    "saturation": -100
+                }, {
+                    "gamma": 0.54
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "water",
+                "stylers": [{
+                    "color": "#4d4946"
+                }]
+            }, {
+                "featureType": "poi",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            }, {
+                "featureType": "road.local",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "simplified"
+                }]
+            }, {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            }, {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{
+                    "gamma": 0.48
+                }]
+            }, {
+                "featureType": "transit.station",
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            }, {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "gamma": 7.18
+                }]
+            }]
+
+        };
+    }
+]);
