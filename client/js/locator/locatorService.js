e95eab739d8a428b7d95c319e60bcef35c2723b3
Melinda Bernardo <melindabernrdo@gmail.com>
2016-06-27 16:07:59 -0700

    [fix] adds firebase url clientside

--- client/js/locator/locatorService.js
+++ client/js/locator/locatorService.js
@@ -1,16 +1,16 @@
 var locator = angular.module('parkAssist.locator');
 var Q = require('q');
-var fb_keys = require('../../../firebaselink.js');
+var fb_keys = "https://parkassistapp.firebaseio.com/";
 
 locator.factory('Locator', ['$http', function ($http) {
 
   var createUser = function (tuple, range) {
     var deferred = Q.defer();
-    
+
     //Create a new user on firebase
-    var fb = new Firebase(fb_keys.url);
+    var fb = new Firebase(fb_keys);
     var dbUser = fb.child('Users').push({ latitude: tuple[0], longitude: tuple[1], range: range });
-    
+
     deferred.resolve(dbUser);
 
     return deferred.promise;
