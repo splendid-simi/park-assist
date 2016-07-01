ed498f313f07ae8eeb9e788ff7f2aa7e202beff7
Melinda Bernardo <melindabernrdo@gmail.com>
2016-06-30 21:42:24 -0700

    [refactor] refactors parking to es6

--- client/js/locator/locatorService.js
+++ client/js/locator/locatorService.js
@@ -1,16 +1,16 @@
 var locator = angular.module('parkAssist.locator');
 var Q = require('q');
-var fb_keys = "https://parkassist-dd5e5.firebaseio.com/";
+var fb_keys = "https://parkassistapp.firebaseio.com/";
 
 locator.factory('Locator', ['$http', function ($http) {
-
+  console.log('creating a new user');
   var createUser = function (tuple, range) {
     var deferred = Q.defer();
 
     //Create a new user on firebase
     var fb = new Firebase(fb_keys);
     var dbUser = fb.child('Users').push({ latitude: tuple[0], longitude: tuple[1], range: range });
-
+    console.log('creating a new user', tuple);
     deferred.resolve(dbUser);
 
     return deferred.promise;
---
+++ server/parking/setParking.js
@@ -0,0 +1,38 @@
+import Firebase from 'firebase'
+import fb_keys from './../config/keys.js'
+import utilities from './../utilities/utilities.js'
+
+let fb = new Firebase(fb_keys.url);
+
+const setLocationParking = (childSnapshot, prevChildKey) => {
+  let user = childSnapshot.val();
+  let userKey = childSnapshot.key();
+
+  //Use the user's coordinates to get a list of feasible spots
+  let radius = user.range;
+  let tuple = [user.latitude, user.longitude];
+
+  //getspots
+  fb.child('MeteredParkingSpots').once('value', (parkingSpots) => {
+
+    let pSpots = parkingSpots.val(); // get all parking meters
+    let closeSpots = [];
+    let freeSpots = {};
+
+    for (var key in pSpots) {
+      let displacement = utilities.getDistance(tuple[0], tuple[1], pSpots[key].latitude, pSpots[key].longitude);
+
+      // if parking spot is in range
+      if (displacement < radius) {
+        pSpots[key].distance = displacement;
+        if (pSpots[key].mostRecentEvent === 'SE') {
+          freeSpots[key] = pSpots[key];
+        }
+      }
+    }
+    //add list of recomendations to User in database
+    fb.child('Users').child(userKey).child('Recommendations').set(freeSpots);
+  });
+}
+
+export default setLocationParking;
