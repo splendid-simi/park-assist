d119b0d69a27820190da99f6c5352c9e3b8b9376
Melinda Bernardo <melindabernrdo@gmail.com>
2016-07-11 19:11:40 -0700

    [feat] calculates NW, SE lat and long points for destination radius

---
+++ destinationCalculations.js
@@ -0,0 +1,35 @@
+address: 604 Arizona Ave, Santa Monica, CA 90401
+lat: 34.019269
+long: -118.494344
+
+1. Take in destination lat + long
+2. North lat + long (B = 0)
+3. NW corner by taking North Lat + Dest Long + (B = 270)
+4. South lat + long (B = 90)
+5. SE corner lat + S long (B = 180)
+
+// Melinda's NW Point:
+Latitude: 34°01′20″N      34.02217069
+Longitude: 118°29′52″W    -118.49782891
+Final bearing: 269°59′53″ 269.99805015
+Back bearing: 89°59′53″   89.99805015
+
+// Melinda's SE point:
+Latitude: 34°01′09″N      34.01926895
+Longitude: 118°29′15″W    -118.48737442
+Final bearing: 90°00′07″   90.00194964
+Back bearing: 270°00′07″   270.00194964
+
+// NW Point
+Latitude: 34°01′30″N      34.02507243
+Longitude: 118°29′52″W    -118.49782903
+Final bearing: 269°59′53″ 269.99804993
+Back bearing: 89°59′53″   89.99804993
+
+//SE Point
+Latitude: 34°00′59″N       34.01636721
+Longitude: 118°29′27″W    -118.49085933
+Final bearing: 90°00′07″   90.00194943
+Back bearing: 270°00′07″   270.00194943
+
+https://data.smgov.net/resource/kn6p-4y74.json?$where=within_box(map_point, 34.02507243,-118.49782903,34.01636721,-118.49085933)
