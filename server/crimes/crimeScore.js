121996d562c167f3a9a7086464eeb306763f7ccd
Melinda Bernardo <melindabernrdo@gmail.com>
2016-07-12 18:00:29 -0700

    [feat] queries SM crime api for crimes in past year within dest location

--- server/crimes/crimeScore.js
+++ server/crimes/crimeScore.js
@@ -11,26 +11,28 @@ let radius = 321.869;
 let destLoc;
 
 const getUserCoords = (childSnapshot, prevChildKey) => {
-  let user = childSnapshot.val();
-  let userKey = childSnapshot.key();
+  // let user = childSnapshot.val();
+  // let userKey = childSnapshot.key();
 
   // calculateDestCoords({"lat": user.latitude, "lon": user.longitude});
   calculateDestCoords({"lat": 34.019269, "lon": -118.494344});
 }
 
-const calculateDestCoords = (destLoc) => {
+const calculateDestCoords = (destCoords) => {
+  console.log('destCoords:', destCoords);
   // calculate the NW and SW corners
-  let northLoc = calcDestPoint(destLoc, radius, 0);
-  let southLoc = calcDestPoint(destLoc, radius, 90);
-  let northwest = calcDestPoint({"lat": northLoc.lat, "lon": destLoc.lon}, radius, 270);
-  let southeast = calcDestPoint({"lat": destLoc.lat,  "lon": southLoc.lon}, radius, 180);
+  let northLoc = calcDestPoint(destCoords, radius, 0);
+  let southLoc = calcDestPoint(destCoords, radius, 90);
+  let northwest = calcDestPoint({"lat": northLoc.lat, "lon": destCoords.lon}, radius, 270);
+  let southeast = calcDestPoint({"lat": destCoords.lat,  "lon": southLoc.lon}, radius, 180);
 
-  return [
+  destLoc = [
     northwest.lat,
     northwest.lon,
     southeast.lat,
     southeast.lon
   ]
+  // getCrimes();
 }
 
 // calc destination using haversine formula
@@ -44,7 +46,7 @@ const calcDestPoint = (coords, radius, bearing) => {
 
 // get crimes that have occurred in past year near dest. location
 const getCrimes = () => {
-  let query = buildCrimeQuery(destLoc);
+  let url = buildCrimeQuery(destLoc);
   request(url, (error, response, body) => {
     if(error) {
       console.log('Error getting data. Error:', error);
@@ -55,17 +57,21 @@ const getCrimes = () => {
   });
 }
 
-  const buildCrimeQuery = (destLoc) => {
-    let url = 'https:data.smgov.net/resource/kn6p-4y74.json?'
-    let date = '$where=date_occurred > ' + '"' + new Date().getFullYear() - 1 + '-01-01' + '"'
-    let coords = 'within_box(map_point,' + destLoc + ')&$order=created_date asce';
+  const buildCrimeQuery = () => {
+    let url = 'https:\/\/data.smgov.net/resource/kn6p-4y74.json?'
+    let date = '$where=date_occurred > ' + '"' + (new Date().getFullYear() - 1) + '-01-01' + '"'
+    let coords = '';
 
-    console.log('query:', url + date + 'AND' + coords);
-    return url + date + 'AND' + coords;
+    if(destLoc) {
+      coords = 'within_box(map_point,' + destLoc + ')&$order=date_occurred desc';
+    }
+
+    console.log('query:', url + date + ' AND ' + coords);
+    return url + date + ' AND ' + coords;
   }
 
   //  store crime score in User table in database
   //   fb.child('Users').child(userKey).child('Crime').set(crimeScore);
   // });
 
-export default calculateDestCoords;
+export default getUserCoords;
